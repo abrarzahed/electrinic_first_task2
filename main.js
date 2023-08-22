60c4521ac8d4b9efb637cddfe050c092d4857117
abrarzahed <abrarzahed6986@gmail.com>
2023-08-22 23:25:58 +0600

    changed favicon

--- main.js
+++ main.js
@@ -1,11 +1,19 @@
 import "./styles/style.scss";
 
+/****************************************** 
+COMMENT: selectors   
+******************************************/
 const sidebar = document.querySelector(".sidebar");
 const leftMenuIcon = document.querySelector(".sidebar-icon");
 const leftMenuCloseIcon = document.querySelector(".sidebar-close");
+
+/****************************************** 
+COMMENT: sidebar controls   
+******************************************/
 leftMenuIcon.addEventListener("click", () => {
   sidebar.classList.toggle("visible");
 });
+
 leftMenuCloseIcon.addEventListener("click", () => {
   sidebar.classList.remove("visible");
 });
