b124ffd4040658a32f70f9e2336d8dd08ae05197
abrarzahed <abrarzahed6986@gmail.com>
2023-08-22 22:38:01 +0600

    fixed issue

--- main.js
+++ main.js
@@ -2,6 +2,10 @@ import "./styles/style.scss";
 
 const sidebar = document.querySelector(".sidebar");
 const leftMenuIcon = document.querySelector(".sidebar-icon");
+const leftMenuCloseIcon = document.querySelector(".sidebar-close");
 leftMenuIcon.addEventListener("click", () => {
   sidebar.classList.toggle("visible");
+});
+leftMenuCloseIcon.addEventListener("click", () => {
+  sidebar.classList.remove("visible");
 });
