72df6a2ffe23b51810978cde564b894265c886a2
abrarzahed <abrarzahed6986@gmail.com>
2023-08-22 20:27:30 +0600

    recent upload and file manager

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
 import "./styles/style.scss";
+
+const sidebar = document.querySelector(".sidebar");
+const leftMenuIcon = document.querySelector(".sidebar-icon");
+leftMenuIcon.addEventListener("click", () => {
+  sidebar.classList.toggle("visible");
+});
